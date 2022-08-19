0bcd8088a37e4a3cc4159870b7a08fa017b9193d
MalzSmith <stssl3780@gmail.com>
2022-08-19 16:44:49 +0200

    oop feladat fix

--- 13_oop_3.py
+++ 13_oop_3.py
@@ -5,29 +5,6 @@
 # Tanár: A Tanár az Ember-ből származik. Tartoznak hozzá diákok, ezt egy listában tárolja. Van neki még egy 'legregebbi' nevű függvénye, ami visszaadja a legkorábban kezdett diákjának a nevét. (Feltéve, hogy a legkorábbi évben csak egy diákja kezdett)
 
 
-class Ember():
-    def __init__(self, nev, azonosito) -> None:
-        self.nev = nev
-        self.azonosito = azonosito
-
-class Diak(Ember):
-    def __init__(self, nev, azonosito, beiratkozas) -> None:
-        super().__init__(nev, azonosito)
-        self.beiratkozas = beiratkozas
-
-class Tanar(Ember):
-    def __init__(self, nev, azonosito, diakok) -> None:
-        super().__init__(nev, azonosito)
-        self.diakok = diakok
-    def legregebbi(self):
-        res = self.diakok[0]
-        m = self.diakok[0].beiratkozas
-        for d in self.diakok:
-            if d.beiratkozas < m:
-                res = d
-                m = d.beiratkozas
-        return res.nev
-
 
 # TESZTEK -------------------------------------------------------------------------------------------------------------------------------------------------
 # A vonal alatt ne szerkeszd a kódot
